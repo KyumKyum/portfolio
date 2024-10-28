e2533fccddd11bdaced597e407c784adb4e237a6
Kyu Min Lim <mycheesepasta@gmail.com>
2024-10-28 20:13:02 +0900

    fix: font-size fix rem

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 import tailwindAnimate from 'tailwindcss-animate';
 
 export default {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    content: ['./index.html', './src/**/*.{html, js,ts,jsx,tsx}'],
     theme: {
         extend: {
             fontFamily: {
