ea0bb0d6e097eeadd902327c070f0d6a78ff5ee1
Kyu Min Lim <mycheesepasta@gmail.com>
2024-11-03 16:42:03 +0900

    fix: simple project card height

--- src/pages/landingPage/components/project/SimpleProjectCard.tsx
+++ src/pages/landingPage/components/project/SimpleProjectCard.tsx
@@ -12,10 +12,10 @@ function SimpleProjectCard({ cardImageUrl, title, description, cb }: SimpleProje
             className="transition ease-in-out delay-150 flex w-[200px] h-[120px] bg-stone-100 shadow-sm rounded-lg hover:scale-110"
             onClick={cb}
         >
-            <div className={'flex justify-center items-center w-2/6 bg-white rounded-l-lg'}>
+            <div className={'flex justify-center items-center w-2/6 h-full bg-white rounded-l-lg'}>
                 <img src={cardImageUrl} alt="banner" className="w-2/3 h-2/3 object-contain rounded-l-lg" />
             </div>
-            <div className="w-4/6 flex flex-col justify-around p-4">
+            <div className="w-4/6 h-full flex flex-col justify-around p-4">
                 <div>
                     <p className="text-[12px] font-Suit font-bold text-start">{title}</p>
                     <p className="text-[10px] font-Suit text-start">{description}</p>
